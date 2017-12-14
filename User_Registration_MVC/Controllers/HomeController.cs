20e73d4bec90df6d78e039445f20e382165acf4b
dawidb1 <33998058+dawidb1@users.noreply.github.com>
2017-12-15 00:20:04 +0100

    Add project files.

---
+++ User_Registration_MVC/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using User_Registration_MVC.Models;
+
+namespace User_Registration_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Registration
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult Index()
+        //{
+
+        //    return View();
+        //}
+
+        
+    }
+}
---
+++ User_Registration_MVC/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using User_Registration_MVC.Models;
+
+namespace User_Registration_MVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        [HttpGet]
+        public ActionResult Rejestration()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Rejestration(User user)
+        {
+            var db = new SleepAppV2Entities();
+            user.CreatedDate = DateTime.Now;
+            db.Users.Add(user);
+            db.SaveChanges();
+            string message = string.Empty;
+            switch (user.UserId)
+            {
+                case -1:
+                    message = "Username already exists.\\nPlease choose a different username.";
+                    break;
+                case -2:
+                    message = "Supplied email address has already been used.";
+                    break;
+                default:
+                    message = "Registration successful.\\nUser Id: " + user.UserId.ToString();
+                    break;
+            }
+            ViewBag.Message = message;
+
+            return View(user);
+        }
+    }
+}
